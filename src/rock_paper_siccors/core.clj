c72392d2fc7f1245b4012c70215aad72132d50f0
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-16 19:51:02 +0100

    Initial commit for rock apper siscorcs bot

---
+++ src/rock_paper_siccors/core.clj
@@ -0,0 +1,6 @@
+(ns rock-paper-siccors.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/rock_paper_siccors/core_test.clj
@@ -0,0 +1,7 @@
+(ns rock-paper-siccors.core-test
+  (:require [clojure.test :refer :all]
+            [rock-paper-siccors.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
